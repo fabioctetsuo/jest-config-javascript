f098834a467b964f50c1a887535af356a81b72f0
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-04-10 18:06:07 -0300

    feat: test an emontion styled ui with custom jest snapshot serializers

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.module\\.css': 'identity-obj-proxy',
     '\\.css$': require.resolve('./tests/test-style-mock.js')
-  }
+  },
+  snapshotSerializers: ['jest-emotion']
 }
