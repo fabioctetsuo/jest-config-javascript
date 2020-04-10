d7afe761eb964855e0add8a39d9eb717b0df54af
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-04-10 13:58:00 -0300

    feat: configuration of moduleNameMapper to run CSS loaders in our jest tests

---
+++ src/shared/__tests__/auto-scaling-text.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AutoScalindText from '../auto-scaling-text';
+
+test('Must render AutoScalingText', () => {
+  render(<AutoScalindText />);
+})
