9db82b61908062fd98343b499a8a46dc181b83f2
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-04-18 21:25:01 -0300

    configuring global code threshold

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,19 @@ module.exports = {
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 32,
+      branches: 18,
+      functions: 32,
+      lines: 34,
+    },
+    './src/shared/utils': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    }
+  },
 }
