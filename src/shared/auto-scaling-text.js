0529dcedde4a359148769cd2b84a9edc3495564b
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-04-19 12:54:29 -0300

    feat: run only relevant jest tests in git commit to avoid breakage

--- src/shared/auto-scaling-text.js
+++ src/shared/auto-scaling-text.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './auto-scaling-text.module.css'
 
+// testing husky + lint-staged for relevant files
+
 function getScale(node) {
   if (!node) {
     return 1
